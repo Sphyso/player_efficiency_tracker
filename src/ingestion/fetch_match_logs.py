eb9df277fe00340b23f710cf4220e38225abb3ee
Sifiso <sifisohadebe0103@gmail.com>
2026-08-31 19:46:01 +0200

    chore: retreiving competitions and wc matches from statsbombpy

--- src/ingestion/fetch_match_logs.py
+++ src/ingestion/fetch_match_logs.py
@@ -1,64 +1,22 @@
-import json
-import os
-from pathlib import Path
-from dotenv import load_dotenv
+import pandas as pd
+from statsbombpy import sb
 
-# Load environment variables from .env
-load_dotenv()
+# Tells Pandas to show every single row and column
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)        # Prevents columns from wrapping to a new line
+pd.set_option('display.max_colwidth', None)
 
-RAW_DATA_DIR = Path("data/raw")
+competitions = sb.competitions()
+# print(competitions)
 
-def fetch_sample_match_log():
-    """
-    Simulates fetching raw match logs (lineups, cards, minutes)
-    and landing them in the raw S3/local data lake directory.
-    """
-    print("🚀 Initializing Match Log Ingestion...")
+matches_2022_wc = sb.matches(competition_id=43, season_id=106)
+clean_matches = matches_2022_wc[[
+    'match_date', 
+    'home_team', 
+    'away_team', 
+    'home_score', 
+    'away_score', 
+]].sort_values(by='match_date')
 
-
-    # Mock payload simulating what a soccer API returns
-    mock_payload = {
-        "match_id": "WC2026_FINAL_01",
-        "stage": "Final",
-        "teams": {
-            "home": "Argentina",
-            "away": "Spain"
-        },
-        "player_events": [
-            {
-                "player_id": 101,
-                "player_name": "Julian Alvarez",
-                "is_starter": True,
-                "minutes_played": 78,
-                "goals": 0,
-                "assists": 0,
-                "yellow_cards": 0,
-                "red_cards": 0,
-                "fouls_committed": 2
-            },
-            {
-                "player_id": 102,
-                "player_name": "Lautaro Martinez",
-                "is_starter": False,
-                "minutes_played": 12,
-                "goals": 0,
-                "assists": 0,
-                "yellow_cards": 1,
-                "red_cards": 0,
-                "fouls_committed": 1
-            }
-        ]
-    }
-
-    # Ensure landing directory exists
-    RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
-    
-    output_path = RAW_DATA_DIR / f"{mock_payload['match_id']}.json"
-    
-    with open(output_path, "w") as f:
-        json.dump(mock_payload, f, indent=2)
-        
-    print(f"✅ Successfully landed raw match log to: {output_path}")
-
-if __name__ == "__main__":
-    fetch_sample_match_log()
+print(matches_2022_wc)
